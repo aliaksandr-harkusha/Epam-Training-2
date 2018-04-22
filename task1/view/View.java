2e11850207d3d0a11d37e5f0bc156a9f5610b12c
aliaksandr-harkusha <38460516+aliaksandr-harkusha@users.noreply.github.com>
2018-04-22 18:06:35 +0300

    Update View.java

--- task1/view/View.java
+++ task1/view/View.java
@@ -1,5 +1,5 @@
 package by.epam.task1.view;
-
+// класс View по сути избыточен - просто пиши System.out.println - в местах, где нужно сделать вывод на консоль
 public class View {
 
     public static void print(Object msg) {
